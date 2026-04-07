d2e35ba8ece1fa9edc89b9aa53d1790fdb6fecd2
Las45 <matteo.morent@gmail.com>
2026-04-07 15:02:05 +0200

    Initial commit

---
+++ SnakeLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Sinks;
+
+namespace Snake
+{
+    public static class SnakeLogger
+    {
+        public static Serilog.Core.Logger logger { get; private set; }
+        public static bool Initialized { get; private set; } = false;
+        public static void init(string logfilename)
+        {
+            logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(logfilename,
+                rollingInterval: RollingInterval.Day,
+                retainedFileCountLimit: 7)
+                .CreateLogger();
+            Log.Information("Hello, Rotating Logs");
+        }
+    }
+}
